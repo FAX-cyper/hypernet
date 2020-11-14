38150c086dfc5c86d5f5bbb5e29e1196532e0361
FAX-cyper <70154295+FAX-cyper@users.noreply.github.com>
2020-11-14 10:16:15 +0800

    Update PersonSubject.java

--- src/main/hypernet/subject/PersonSubject.java
+++ src/main/hypernet/subject/PersonSubject.java
@@ -34,10 +34,10 @@ public abstract class PersonSubject extends IntelSubject {
     protected abstract List<PersonFilter> getFilters();
 
     private void addBasicInfo(TooltipMakerAPI info, int adminsSize) {
-        String isOrAre = adminsSize == 1 ? " is " : " are ";
-        String numberOrNo = adminsSize == 0 ? "no " : adminsSize + " ";
-        String emptyOrS = adminsSize == 1 ? "" : "s";
-        String basicInfo = "There" + isOrAre + numberOrNo + entity + emptyOrS + " present on " + market.getName()
+        String isOrAre = adminsSize == 1 ? " 显示" : " 显示";
+        String numberOrNo = adminsSize == 0 ? "没有 " : adminsSize + " ";
+        String emptyOrS = adminsSize == 1 ? "" : "";
+        String basicInfo = "检索" + isOrAre + numberOrNo + entity + emptyOrS + "现在位于" + market.getName()
                 + ". ";
         super.addBasicInfo(info, basicInfo);
     }
@@ -46,10 +46,10 @@ public abstract class PersonSubject extends IntelSubject {
         List<SkillLevelAPI> skills = person.getStats().getSkillsCopy();
         CollectionHelper.reduce(skills, new SkillLevelFilter());
         int skillSize = skills.size();
-        String numberOrNo = skillSize == 0 ? "no " : skillSize + " ";
-        String skillOrSkills = skillSize == 1 ? "skill" : "skills";
+        String numberOrNo = skillSize == 0 ? "没有" : skillSize + " ";
+        String skillOrSkills = skillSize == 1 ? "技能" : "技能";
         String dotOrColon = skillSize == 0 ? "." : ":";
-        String adminPara = "%s, level " + person.getStats().getLevel() + " " + entity + " with " + numberOrNo
+        String adminPara = "%s," + person.getStats().getLevel() + "级" + entity + " 带有 " + numberOrNo
                 + skillOrSkills + dotOrColon;
         info.addPara("", 0f);
         info.addPara(adminPara, 10f, Misc.getTextColor(), Misc.getHighlightColor(), person.getNameString());
@@ -61,7 +61,7 @@ public abstract class PersonSubject extends IntelSubject {
             float skillLevel = skill.getLevel();
             SkillSpecAPI skillSpec = skill.getSkill();
             TooltipMakerAPI text = info.beginImageWithText(skillSpec.getSpriteName(), 14f);
-            text.addPara(skillSpec.getName() + ", level " + Math.round(skillLevel), 0f);
+            text.addPara(skillSpec.getName() + ", " + Math.round(skillLevel) + "级", 0f);
             info.addImageWithText(3f);
         }
     }
